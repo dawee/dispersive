dc0a3ea521e5518a67ac2bf865fd533e10724048
David Corticchiato <david.corticchiato@gmail.com>
2016-07-01 13:08:26 +0200

    replace Action.createAsync by promise management

--- test/action.test.js
+++ test/action.test.js
@@ -1,9 +1,23 @@
 const assert = require('assert');
+const sinon = require('sinon');
 const Dispersive = require('..');
+const Dispatcher = require('../lib/dispatcher');
+
 
 describe('Action', () => {
 
-  it('should trigger immediately after');
+  it('should trigger immediately after', () => {
+    const listener = sinon.spy();
+    const action = Dispersive.Action.create(
+      (value) => ({value})
+    );
+
+    Dispatcher.main().on(action, listener);
+    action(42);
+    assert(listener.called);
+    assert.equal(42, listener.getCall(0).args[0].value);
+  });
+ 
   it('should trigger asynchronously');
   it('should chain actions');
 
