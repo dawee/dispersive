67ae2010544995878ac1f5d3cc0f6e3897316a64
David Corticchiato <david.corticchiato@gmail.com>
2016-07-01 18:07:25 +0200

    fix ObjectsManager

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,9 +1,24 @@
 const assert = require('assert');
 const Dispersive = require('..');
+const ObjectsManager = require('../lib/manager');
 
 describe('Action', () => {
 
-  it('create a new entry');
-  it('delete a given entry');
+  it('create a new entry', () => {
+    const manager = new ObjectsManager();
+
+    manager.create({id: 42});
+    assert.equal(manager.values[0].id, 42);
+  });
+
+
+  it('delete a given entry', () => {
+    const manager = new ObjectsManager();
+    const entry = {id: 42};
+
+    manager.create(entry);
+    manager.delete(entry);
+    assert.equal(manager.values.length, 0);
+  });
 
 })
