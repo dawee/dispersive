c2e2532762f8603212aa3a523f079dce564acdce
David Corticchiato <david.corticchiato@gmail.com>
2016-07-05 14:58:41 +0200

    use DISPERSIVE_ECMA env variable to let user choose version

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ let Action;
 let Dispatcher;
 let Store;
 
-if (require('is-browser')) {
+if (process.env.DISPERSIVE_ECMA === '5') {
+  Action = require('./lib/action');
+  Dispatcher = require('./lib/dispatcher');
+  Store = require('./lib/store');
+} else {
   require("babel-core/register");
   require("babel-polyfill");
   Action = require('./es5/lib/action');
   Dispatcher = require('./es5/lib/dispatcher');
   Store = require('./es5/lib/store');
-} else {
-  Action = require('./lib/action');
-  Dispatcher = require('./lib/dispatcher');
-  Store = require('./lib/store');
 }
 
 exports.createStore = (dispatcher) => new Store(dispatcher);
 exports.createAction = Action.create;
 exports.createActionGroup = Action.createGroup;
 exports.createDispatcher = () => new Dispatcher();
-
