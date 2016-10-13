65fe1ce9fcefedec4e7bb2029329dd33117f9d40
David Corticchiato <david.corticchiato@gmail.com>
2016-10-13 10:40:06 +0200

    add header

--- examples/movies/lib/component/App.js
+++ examples/movies/lib/component/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MoviesWall from './MoviesWall';
+import Header from './header';
 import actions from '../actions';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <MoviesWall />
       </div>
     );
